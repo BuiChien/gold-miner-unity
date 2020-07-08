6df0aae8c1adfc51b869e56db2261bca1c5d7060
chienbv <chien.buivan@hotmail.com>
2020-07-09 06:53:24 +0700

    prepare interface to spawn good

---
+++ Assets/Scripts/Main/GoodsSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodsSpawner : MonoBehaviour, ISpawner {
+  [SerializeField]
+  private GoodsSo[] list_goods_;
+  public List<GoodsSo> GoodsSpawned;
+  public void Spawn(int level) {
+
+  }
+
+  public void Clear() {
+
+  }
+}
---
+++ Assets/Scripts/Main/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+  // Start is called before the first frame update
+  GridLayout grid_layout_;
+
+  void Start() {
+    gameObject.AddComponent<GridLayout>();
+    grid_layout_ = gameObject.GetComponent<GridLayout>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
